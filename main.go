d94e93b6be02a3ebacb341b797892a3e01dd76f1
dplamenov <dimitar.plamenov@gmail.com>
2021-03-21 14:58:41 +0200

    serve static html
    serve static html

--- main.go
+++ main.go
@@ -1,25 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+func home(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "templates/home.html")
 }
 
-func headers(w http.ResponseWriter, req *http.Request) {
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
+func about(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "templates/about.html")
 }
+
 
 func main() {
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
 
 	http.ListenAndServe(":8090", nil)
 }
